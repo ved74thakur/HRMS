1b38e080399b9e7d56da5ba8496bf55e96426e67
ved74Thakur <ved74thakur@gmail.com>
2023-10-18 11:10:08 +0530

    Single Employee

--- leaveApplication2/Repostories/EmployeeRepository.cs
+++ leaveApplication2/Repostories/EmployeeRepository.cs
@@ -32,11 +32,14 @@ namespace leaveApplication2.Repostories
 
         public async Task<Employee> GetEmployeeByIdAsync(long id)
         {
-            var singleEmployee =  await _context.Employees.FindAsync(id);
+            
+            var singleEmployee = await _context.Employees.Include(e => e.Designation).Include(e => e.Gender).AsNoTracking().FirstOrDefaultAsync(e => e.employeeId == id);
+           
             if (singleEmployee == null) 
             {
                 return null;
             }
+            
             return singleEmployee;
         }
 
