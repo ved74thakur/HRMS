9fdaf0d9c8429e143c19709765bef98871f6da59
ved74Thakur <ved74thakur@gmail.com>
2023-11-01 10:29:52 +0530

    Final changes

---
+++ leaveApplication2/Migrations/20231010045806_changedCodeClass.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace leaveApplication2.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedCodeClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ActivationToken",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActivationToken",
+                table: "Employees");
+        }
+    }
+}
