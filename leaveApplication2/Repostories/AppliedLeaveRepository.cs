50375282873e2ee4958808fe5db3a311787211f3
ved74Thakur <ved74thakur@gmail.com>
2023-11-03 16:17:22 +0530

    changes

--- leaveApplication2/Repostories/AppliedLeaveRepository.cs
+++ leaveApplication2/Repostories/AppliedLeaveRepository.cs
@@ -177,7 +177,7 @@ namespace leaveApplication2.Repostories
         {
             // Replace the condition with your specific criteria
             var unapprovedLeaves = await _context.AppliedLeaves
-    .Where(leave => leave.employeeId == appliedLeave.employeeId && leave.IsApproved == appliedLeave.IsApproved)
+    .Where(leave => leave.employeeId == appliedLeave.employeeId && leave.IsApproved == appliedLeave.IsApproved && appliedLeave.IsRejected == false)
     .ToListAsync();
 
             return unapprovedLeaves;
