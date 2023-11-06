9995604363958f0ea22ad772dcbfc42d3836db93
ved74Thakur <ved74thakur@gmail.com>
2023-11-06 11:31:28 +0530

    new file added test repository

---
+++ leaveApplication2/Repostories/TestRepository.cs
@@ -0,0 +1,9 @@
+﻿using leaveApplication2.Data;
+
+namespace leaveApplication2.Repostories
+{
+    public class TestRepository
+    {
+        private readonly ApplicationDbContext _context;
+    }
+}
