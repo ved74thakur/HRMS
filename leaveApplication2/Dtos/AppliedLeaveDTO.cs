203788a16a2278359578a629771032e17c33628b
lokhandecoder <amit.lokhande@wonderbiz.in>
2023-11-08 10:12:31 +0530

    changes

---
+++ leaveApplication2/Dtos/AppliedLeaveDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace leaveApplication2.Dtos
+{
+    public class AppliedLeaveDTO
+    {
+        public long appliedLeaveTypeId {  get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string LeaveTypeName { get; set; } = string.Empty;
+        public double BalanceLeave { get; set;}
+        public double AppliedLeave { get; set; }
+        public double RemaingLeave { get; set; }
+        public string LeaveReason { get; set; } = string.Empty;
+         public double ApplyLeaveDay { get; set; } 
+        public bool isApproved {  get; set; }
+
+        public bool isRejected { get; set; }
+
+
+    }
+}
