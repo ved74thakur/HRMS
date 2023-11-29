fcd043343374842b2cb2931a6024c262903bbfee
Ved Thakur <SESA744803@se.com>
2023-11-29 11:03:49 +0530

    Added changes C

--- leaveApplication2/Services/LeaveReportService.cs
+++ leaveApplication2/Services/LeaveReportService.cs
@@ -2,7 +2,6 @@
 using leaveApplication2.Models;
 using leaveApplication2.Repostories;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections;
 using System.Linq.Expressions;
 
 namespace leaveApplication2.Services
@@ -10,13 +9,13 @@ namespace leaveApplication2.Services
     public class LeaveReportService : ILeaveReportService
     {
         private readonly IAppliedLeaveRepository _leaveRepository;
-      
+
         public LeaveReportService(IAppliedLeaveRepository leaveRepository)
         {
             _leaveRepository = leaveRepository;
         }
 
-     
+
 
         public async Task<IEnumerable<AppliedLeave>> GetLeavesReportAsync([FromBody] LeaveReport leaveReport)
         {
@@ -25,7 +24,7 @@ namespace leaveApplication2.Services
                 throw new InvalidOperationException("Start date cannot be greater than end date.");
             }
 
-      
+
 
             Expression<Func<AppliedLeave, bool>> filter;
             filter = la => la.StartDate >= leaveReport.startDate
@@ -37,8 +36,8 @@ namespace leaveApplication2.Services
             }
 
             var leavesReport = await _leaveRepository.GetAppliedLeavesAsync(filter);
-            var orderedLeavesReport = leavesReport.OrderByDescending(la => la.StartDate).ThenBy(la => la.Employee.firstName);
-           
+            var orderedLeavesReport = leavesReport.OrderByDescending(la => la.StartDate).ThenBy(la => la.Employee.firstName, StringComparer.OrdinalIgnoreCase);
+
             return orderedLeavesReport;
         }
 
